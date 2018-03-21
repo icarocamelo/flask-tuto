c69e9c7b34396739dbee2f0676b473f5a2826871
Icaro Camelo <Icaro.Camelo@emc.com>
2018-03-21 17:08:43 -0400

    Added version

--- app/server.py
+++ app/server.py
@@ -3,17 +3,22 @@ from flask import request
 
 app = Flask(__name__)
 
+app_version = "0.0.1"
+
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
 
-@app.route('/test')
+@app.route('/echo')
 def echo():
     value = request.args.get('value')
 
     return "Echoing... [" + value + "]"
 
+@app.route('/version')
+def version():
+    return 'Version: ' + app_version
 
 if __name__ == '__main__':
     app.run(debug = True)
