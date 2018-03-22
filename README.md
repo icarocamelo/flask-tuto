d2dd626c1cb74fbc97295ef0ae143d0705adcb83
Icaro Camelo <Icaro.Camelo@emc.com>
2018-03-22 15:30:11 -0400

    Basic HTTP auth

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 # Basic REST API with Flask
-https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
+Simple REST API with SSL and Basic HTTP Authentication
 
 ## Install all dependencies
 - pip install -r requirements.txt
 
 ## Update depedencies from code
 - pip freeze > requirements.txt
-
-## Install SSL certificate (optional)
-- pip install pyopenssl
 
 ## Export FLASK_APP env variable
 - export FLASK_APP=app/server.py
@@ -18,7 +15,12 @@ https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
 
 - Example: python app/server.py True True
 
+## Dependencies
+- pip install flask-httpauth
+
 ## Useful resources
+- Flask
+    - https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
 - Flask HTTP auth
     - https://flask-httpauth.readthedocs.io/en/latest/
     - http://polyglot.ninja/securing-rest-apis-basic-http-authentication-python-flask/
--- app/server.py
+++ app/server.py
@@ -1,23 +1,39 @@
 from flask import Flask
 from flask import request
+from flask_httpauth import HTTPBasicAuth
 import sys
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "admin": "admin",
+    "guest": "guest"
+}
 
 app_version = "0.0.1"
 
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
+
 @app.route('/')
 @app.route('/index')
+@auth.login_required
 def index():
-    return "Hello, World!"
+    return "Hello, %s!" % auth.username()
 
 @app.route('/echo')
+@auth.login_required
 def echo():
     value = request.args.get('value')
 
     return "Echoing... [" + value + "]"
 
 @app.route('/version')
+@auth.login_required
 def version():
     return 'Version: ' + app_version
 
@@ -25,7 +41,7 @@ if __name__ == '__main__':
     debug = sys.argv[1]
     ssl = sys.argv[2]
 
-    print ('Starting server... DEBUG enabled: ' + debug)
+    print ('Starting server...')
 
     if ssl == 'True':
         print ('SSL enabled')
